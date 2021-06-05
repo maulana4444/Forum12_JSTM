2710d3ce2a5f0fddf356ee3e1e91b07ceba51447
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-06-05 08:03:29 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,11 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js'); //Predict
-const cls_model = require('./sdk/model.js'); //cls
+const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1886757664:AAER5X02MM9d7RgTQQ4bJ_Xl6mwoefLuVwQ'
+const token = '1566520999:AAG5DMEg1XgTEDULZBFxO41hcCWGL6MIb_0'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -30,6 +29,49 @@ bot.onText(/\/predict/, (msg) => {
     );   
     state = 1
 });
+
+
+
+bot.on('message', (msg) => {
+	if(state == 1){
+		console.log(msg.Text);
+		s = msg.text.split("|");
+		i = s[0]
+		v = s[1]
+		model.predict(
+			[
+				parseFloat(s[0]),
+				parseFloat(s[1])
+			]
+		).then((jres)=>{
+			bot.sendMessage(
+				msg.chat.id,
+				`nilai v yang diprediksi adalah ${jres[0]} volt`
+        );
+        bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+        );
+    })
+		state = 0
+    }else {
+        state = 0 
+        }
+})
+
+// routers
+r.get('/prediction/:i/:r', function(req, res, next) {    
+    model.predict(
+        [
+            parseFloat(req.params.i), // string to float
+            parseFloat(req.params.r)
+        ]
+    ).then((jres)=>{
+        res.json(jres);
+    })
+});
+
+module.exports = r;
 
 
 
