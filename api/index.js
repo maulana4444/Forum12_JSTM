cc2f36d66db4024057c8e8e0bffcee94e78eaee8
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-06-05 09:13:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js');
+const model = require('./sdk/model.js'); //predic
+const cls_model = require ('./sdk/cls_model.js'); //cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1886757664:AAER5X02MM9d7RgTQQ4bJ_Xl6mwoefLuVwQ'
 const bot = new TelegramBot(token, {polling: true});
 
-
+state = 0;
 // Main Menu Bot
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
@@ -17,10 +18,11 @@ bot.onText(/\/start/, (msg) => {
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
         click /predict`
-    );   
+    );
+	state = 0;
 });
 
-state = 0;
+//input requires i dan r 
 bot.onText(/\/predict/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
@@ -34,80 +36,35 @@ bot.onText(/\/predict/, (msg) => {
 
 bot.on('message', (msg) => {
 	if(state == 1){
-		console.log(msg.Text);
-		s = msg.text.split("|");
-		i = s[0]
-		v = s[1]
-		model.predict(
-			[
-				parseFloat(s[0]),
-				parseFloat(s[1])
-			]
-		).then((jres)=>{
-			bot.sendMessage(
-				msg.chat.id,
-				`nilai v yang diprediksi adalah ${jres[0]} volt`
-        );
-        bot.sendMessage(
-                msg.chat.id,
-                `nilai p yang diprediksi adalah ${jres[1]} watt`
-        );
-    })
-		state = 0
-    }else {
-        state = 0 
-        }
-})
-
-// routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
-        ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
-});
-
-module.exports = r;
-
-
-
-bot.on('message', (msg) => {
-	if(state == 1){
-		console.log(msg.Text);
 		s = msg.text.split("|");
 		i = parseFloats(s[0])
 		v = parseFLoat(s[1])
         
 		model.predict(
 			[
-				i,
+				i, // string to float
 				r
 			]
 		).then((jres1)=>{
-            v = parseFloatjres1[0]
-            p = parseFloatjres1[1]
-            
-            cls_model.classify([i, r, v, p]).then((jres2)=> { 
-            
-			bot.sendMessage(
-				msg.chat.id,
-				`nilai v yang diprediksi adalah ${jres[0]} volt`
-        );
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai p yang diprediksi adalah ${jres[1]} watt`
-        );
-            bot.sendMessage(
-				msg.chat.id,
-				`Klasifikasi Tegangan ${jres[2]} volt`
-        );
+			v = parseFloat(jres1[0])
+			p = parseFloat(jres1[1])
+			
+			cls_model.classify([i, r, v, p]).then((jres2)=> {
+				bot.sendMessage(
+					msg.chat.id,
+					`nilai v yang diprediksi adalah ${v} volt`
+				);
+				bot.sendMessage(
+					msg.chat.id,
+					`nilai p yang diprediksi adalah ${p} watt`
+				);
+				bot.sendMessage(
+					msg.chat.id,
+					`Klasifikasi Tegangan ${jres2}`
+				);
+		
         })
     })
-		state = 0
     }else {
         state = 0 
         }
